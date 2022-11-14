181474932ba8e617e636816626531e28511aff9a
y5hn4v <114895749+y5hn4v@users.noreply.github.com>
2022-11-14 10:01:28 +0530

    Update bruteforce.py
    -Added conditions to check if the files and path actually exist
    -Added condition to check if the file is actually .zip
    -Added more comments

--- bruteforcer.py
+++ bruteforcer.py
@@ -1,7 +1,7 @@
 
 # Zip File Password Bruteforcer Made in Python
 # Author : y5hn4v (https://github.com/y5hn4v)
-# Version : 1.0.0
+# Version : 1.1.0
 # For educational purpose only
 # To display the help message : python bruteforcer.py -h
 
@@ -20,38 +20,51 @@ def main():
     parser.add_argument("-s", metavar="Extract/to/path", type=str, help="Path to extract the zip file.If not provided, the file will be extracted to current working directory")
 
     args = parser.parse_args()
-
+    
+    #File locations and the save path
     plist = args.PasswordList
     zip_file = args.ZipFile
     save_file = args.s
-
-    if save_file == "":
-        save_file = os.getcwd() #Getting the current working directory
-
-    zp = zipfile.ZipFile(zip_file)
-
-    if zipfile.BadZipFile == True: #Checking if the file is zip
-        print("It is not a zip file. Exiting program")
+    
+    #Below conditions check if the file or the path exists in the users computer
+    if os.path.exists(zip_file) == False:
+        print("Zip File not found")
         exit()
-    else:
-        control = False
-        print('Starting BruteForce: \n')
-        with open(plist, 'rb') as file:
-            if os.path.getsize(plist) == 0: #The password list is empty
-                print("Please provide values in the password list you provided.Exiting program")
-                exit()
-            else:
-                for password in file:
-                    try:
-                        zp.extractall(pwd = password[:-1], path=save_file)
-                        print(f'Found password : {password.decode()}')
-                        control = True
-                        quit()
-                    except Exception as e:
-                        pass
-                if control == False:
-                    print("Password not detected in the password list ")
-
+    if os.path.exists(plist) == False:
+        print("The password list is not found")
+        exit()
+    #If the user didnt specify a path to extract the zip
+    if save_file == None: 
+        save_file = os.getcwd() #Getting the current working directory
+    if os.path.exists(save_file) == False:
+        print("The path to save location does not exists")
+        exit()
+   
+    try:
+        # If the file is actually a zip file this code with execute
+        with zipfile.ZipFile(zip_file) as zp: 
+            control = False
+            print('Starting BruteForce: \n')
+            with open(plist, 'rb') as file:
+                if os.path.getsize(plist) == 0: #The password list is empty
+                    print("Please provide values in the password list you provided.Exiting program")
+                    exit()
+                else:
+                    # Looping through each password in the list
+                    for password in file: 
+                        try:
+                            zp.extractall(pwd = password[:-1], path=save_file)
+                            print(f'Found password : {password.decode()}')
+                            control = True
+                            quit()
+                        except Exception as e: # Password didnt match 
+                            pass
+                    if control == False:
+                        print("Password not detected in the password list ")
+    except zipfile.BadZipFile: # The file is not a zip file
+        print("The file is not a zipfile")
+        exit()
+        
             
 
 if __name__ == '__main__':
